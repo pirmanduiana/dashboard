6d0c9bdb7ef14d39be6244b4aa37ee52bcfd6824
Chandrayana Putra <sherlock_666@live.com>
2018-07-16 14:12:21 +0800

    dari candra

--- protected/resources/views/visimisi.blade.php
+++ protected/resources/views/visimisi.blade.php
@@ -1,18 +1,137 @@
-<div class="demo_container">
-    <div class="demo_container__items">        
-        <div class="material_ripple demo_container__item btn" data-color="#39a1f4">
-            Button
+{{ Html::style('assets/pages/css/base.css') }}
+
+
+
+        <div id="template-page-content">
+            
+            <h1 class="template-page-header text-center"><span>Visi</span></h1>
+                <h3 class="template-page-subheader text-center"><font color="white">Memantapkan Arah Pembangunan Badung Berlandaskan Tri Hita Karana
+                    <br>
+                    <span>Menuju Masyarakat Yang Maju, Damai Dan Sejahtera</span>
+                </h3>
+
+            <h1 class="template-page-header text-center"><span>Misi</span></h1>
+            
+            <div class="template-timeline template-clear-fix">
+                <div class="template-timeline-date">
+                    <h5><font color="white">
+                        2016
+                    </h5>
+                </div>
+                <div class="template-timeline-content template-clear-fix">
+                    <div class="template-timeline-content-column-left">
+                        <ul class="template-timeline-event-list">
+                            <li>
+                                <div class="template-event-list-content">
+                                    <span class="template-icon-small template-icon-info">
+                                        <span class="template-event-list-content-icon-arrow"></span>
+                                        <span class="template-event-list-content-icon-circle"></span>
+                                    </span>
+                                    <h3 class="template-event-list-content-date text-right">1</h3>
+                                    <h3 class="template-event-list-content-header"></h3>
+                                    <p>Memperkokoh Kerukunan Hidup Bermasyarakat Dalam Jalinan Keragaman Adat, Budaya, dan Agama</p>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="template-event-list-content">
+                                    <span class="template-icon-small template-icon-info">
+                                        <span class="template-event-list-content-icon-arrow"></span>
+                                        <span class="template-event-list-content-icon-circle"></span>
+                                    </span>
+                                    <h3 class="template-event-list-content-date text-right">3</h3>
+                                    <h3 class="template-event-list-content-header"></h3>
+                                    <p>Memantapkan Tata Kelola Pemerintah Dengan Menerapkan Prinsip Good Governance dan Clean Governance</p>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="template-event-list-content">
+                                    <span class="template-icon-small template-icon-info">
+                                        <span class="template-event-list-content-icon-arrow"></span>
+                                        <span class="template-event-list-content-icon-circle"></span>
+                                    </span>
+                                    <h3 class="template-event-list-content-date text-right">5</h3>
+                                    <h3 class="template-event-list-content-header"></h3>
+                                    <p>Memperkuat Usaha Mikro Kecil dan Menengah (UMKM) Sebagai Pilar Ekonomi Kerakyatan</p>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="template-event-list-content">
+                                    <span class="template-icon-small template-icon-info">
+                                        <span class="template-event-list-content-icon-arrow"></span>
+                                        <span class="template-event-list-content-icon-circle"></span>
+                                    </span>
+                                    <h3 class="template-event-list-content-date text-right">7</h3>
+                                    <h3 class="template-event-list-content-header"></h3>
+                                    <p>Meningkatkan Perlindungan dan Pengelolaan Terhadap Sumber Daya Alam dan Lingkungan Hidup</p>
+                                </div>
+                            </li>
+                            <li class="template-timeline-event-list-featured">
+                                <div class="template-event-list-content">
+                                    <span class="template-icon-small template-icon-info">
+                                        <span class="template-event-list-content-icon-arrow"></span>
+                                        <span class="template-event-list-content-icon-circle"></span>
+                                    </span>
+                                    <h3 class="template-event-list-content-date text-right">9</h3>
+                                    <h3 class="template-event-list-content-header"></h3>
+                                    <p>Memperkuat Perlindungan dan Pengelolaan Terhadap Sumber Daya Alam dan Lingkungan Hidup</p>
+                                </div>
+                            </li>   
+                        </ul>
+                    </div>
+                    <br>
+                    <div class="template-timeline-content-column-right">
+                        <ul class="template-timeline-event-list">
+                            <li>
+                                <div class="template-event-list-content">
+                                    <span class="template-icon-small template-icon-info">
+                                        <span class="template-event-list-content-icon-arrow"></span>
+                                        <span class="template-event-list-content-icon-circle"></span>
+                                    </span>
+                                    <h3 class="template-event-list-content-date">2</h3>
+                                    <h3 class="template-event-list-content-header"></h3>
+                                    <p>Memantapkan Kualitas Pelayanan Publik Dengan Menerapkan Teknologi Informasi dan Komunikasi</p>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="template-event-list-content">
+                                    <span class="template-icon-small template-icon-info">
+                                        <span class="template-event-list-content-icon-arrow"></span>
+                                        <span class="template-event-list-content-icon-circle"></span>
+                                    </span>
+                                    <h3 class="template-event-list-content-date">4</h3>
+                                    <h3 class="template-event-list-content-header"></h3>
+                                    <p>Meningkatkan Pelaksanaan Program Keluarga Berencana (KB) Dalam Pengelolaan Kependudukan</p>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="template-event-list-content">
+                                    <span class="template-icon-small template-icon-info">
+                                        <span class="template-event-list-content-icon-arrow"></span>
+                                        <span class="template-event-list-content-icon-circle"></span>
+                                    </span>
+                                    <h3 class="template-event-list-content-date">6</h3>
+                                    <h3 class="template-event-list-content-header"></h3>
+                                    <p>Mewujudkan Tatanan Kehidupan Bermasyarakat Yang Menjunjung Tinggi Penegakan Hukum dan Hak Asasi Manusia (HAM)</p>
+                                </div>
+                            </li>
+                            <li class="template-timeline-event-list-featured">
+                                <div class="template-event-list-content">
+                                    <span class="template-icon-small template-icon-info">
+                                        <span class="template-event-list-content-icon-arrow"></span>
+                                        <span class="template-event-list-content-icon-circle"></span>
+                                    </span>
+                                    <h3 class="template-event-list-content-date">8</h3>
+                                    <h3 class="template-event-list-content-header"></h3>
+                                    <p>Memperkuat Daya Saing Daerah Melalui Peningkatan Mutu Sumber Daya Manusia dan Infrastruktur Wilayah</p>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>                  
+                </div>
+                <div class="template-timeline-date">
+                    <h5><font color="white">
+                        2021
+                    </h5>
+                </div>
+            </div>
         </div>
-        <div class="material_ripple demo_container__item btn btn--blue">
-            Button 
-        </div>
-    </div>
-    <div class="demo_container__items">        
-        <div class="material_ripple demo_container__item btn" data-color="#FF4081">
-            Button
-        </div>
-        <div class="material_ripple demo_container__item btn btn--colored">
-            Button 
-        </div>
-    </div>
-</div>
