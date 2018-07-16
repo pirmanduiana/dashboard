216504523a171eea05bb38e983cce2027692c3f1
Chandrayana Putra <sherlock_666@live.com>
2018-07-16 14:48:57 +0800

    edit font visi dan misi

--- protected/resources/views/visimisi.blade.php
+++ protected/resources/views/visimisi.blade.php
@@ -14,7 +14,7 @@
             
             <div class="template-timeline template-clear-fix">
                 <div class="template-timeline-date">
-                    <h5><font color="white">
+                    <h5 style="color:white;">
                         2016
                     </h5>
                 </div>
@@ -29,7 +29,7 @@
                                     </span>
                                     <h3 class="template-event-list-content-date text-right">1</h3>
                                     <h3 class="template-event-list-content-header"></h3>
-                                    <p>Memperkokoh Kerukunan Hidup Bermasyarakat Dalam Jalinan Keragaman Adat, Budaya, dan Agama</p>
+                                    <h3 style="color:white;">Memperkokoh Kerukunan Hidup Bermasyarakat Dalam Jalinan Keragaman Adat, Budaya, dan Agama</h3>
                                 </div>
                             </li>
                             <li>
@@ -40,7 +40,7 @@
                                     </span>
                                     <h3 class="template-event-list-content-date text-right">3</h3>
                                     <h3 class="template-event-list-content-header"></h3>
-                                    <p>Memantapkan Tata Kelola Pemerintah Dengan Menerapkan Prinsip Good Governance dan Clean Governance</p>
+                                    <h3 style="color:white;">Memantapkan Tata Kelola Pemerintah Dengan Menerapkan Prinsip Good Governance dan Clean Governance</h3>
                                 </div>
                             </li>
                             <li>
@@ -51,7 +51,7 @@
                                     </span>
                                     <h3 class="template-event-list-content-date text-right">5</h3>
                                     <h3 class="template-event-list-content-header"></h3>
-                                    <p>Memperkuat Usaha Mikro Kecil dan Menengah (UMKM) Sebagai Pilar Ekonomi Kerakyatan</p>
+                                    <h3 style="color:white;">Memperkuat Usaha Mikro Kecil dan Menengah (UMKM) Sebagai Pilar Ekonomi Kerakyatan</h3>
                                 </div>
                             </li>
                             <li>
@@ -62,7 +62,7 @@
                                     </span>
                                     <h3 class="template-event-list-content-date text-right">7</h3>
                                     <h3 class="template-event-list-content-header"></h3>
-                                    <p>Meningkatkan Perlindungan dan Pengelolaan Terhadap Sumber Daya Alam dan Lingkungan Hidup</p>
+                                    <h3 style="color:white;">Meningkatkan Perlindungan dan Pengelolaan Terhadap Sumber Daya Alam dan Lingkungan Hidup</h3>
                                 </div>
                             </li>
                             <li class="template-timeline-event-list-featured">
@@ -73,7 +73,7 @@
                                     </span>
                                     <h3 class="template-event-list-content-date text-right">9</h3>
                                     <h3 class="template-event-list-content-header"></h3>
-                                    <p>Memperkuat Perlindungan dan Pengelolaan Terhadap Sumber Daya Alam dan Lingkungan Hidup</p>
+                                    <h3 style="color:white;">Memperkuat Perlindungan dan Pengelolaan Terhadap Sumber Daya Alam dan Lingkungan Hidup</h3>
                                 </div>
                             </li>   
                         </ul>
@@ -89,7 +89,7 @@
                                     </span>
                                     <h3 class="template-event-list-content-date">2</h3>
                                     <h3 class="template-event-list-content-header"></h3>
-                                    <p>Memantapkan Kualitas Pelayanan Publik Dengan Menerapkan Teknologi Informasi dan Komunikasi</p>
+                                    <h3 style="color:white;">Memantapkan Kualitas Pelayanan Publik Dengan Menerapkan Teknologi Informasi dan Komunikasi</h3>
                                 </div>
                             </li>
                             <li>
@@ -100,7 +100,7 @@
                                     </span>
                                     <h3 class="template-event-list-content-date">4</h3>
                                     <h3 class="template-event-list-content-header"></h3>
-                                    <p>Meningkatkan Pelaksanaan Program Keluarga Berencana (KB) Dalam Pengelolaan Kependudukan</p>
+                                    <h3 style="color:white;">Meningkatkan Pelaksanaan Program Keluarga Berencana (KB) Dalam Pengelolaan Kependudukan</h3>
                                 </div>
                             </li>
                             <li>
@@ -111,7 +111,7 @@
                                     </span>
                                     <h3 class="template-event-list-content-date">6</h3>
                                     <h3 class="template-event-list-content-header"></h3>
-                                    <p>Mewujudkan Tatanan Kehidupan Bermasyarakat Yang Menjunjung Tinggi Penegakan Hukum dan Hak Asasi Manusia (HAM)</p>
+                                    <h3 style="color:white;">Mewujudkan Tatanan Kehidupan Bermasyarakat Yang Menjunjung Tinggi Penegakan Hukum dan Hak Asasi Manusia (HAM)</h3>
                                 </div>
                             </li>
                             <li class="template-timeline-event-list-featured">
@@ -122,14 +122,14 @@
                                     </span>
                                     <h3 class="template-event-list-content-date">8</h3>
                                     <h3 class="template-event-list-content-header"></h3>
-                                    <p>Memperkuat Daya Saing Daerah Melalui Peningkatan Mutu Sumber Daya Manusia dan Infrastruktur Wilayah</p>
+                                    <h3 style="color:white;">Memperkuat Daya Saing Daerah Melalui Peningkatan Mutu Sumber Daya Manusia dan Infrastruktur Wilayah</h3>
                                 </div>
                             </li>
                         </ul>
                     </div>                  
                 </div>
                 <div class="template-timeline-date">
-                    <h5><font color="white">
+                    <h5 style="color:white;">
                         2021
                     </h5>
                 </div>
